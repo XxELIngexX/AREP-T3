5c594d0f74b3350996d0dd8d27e72db0d0376c52
XxELIngexX <114599685+XxELIngexX@users.noreply.github.com>
2025-08-26 10:53:00 -0500

    Add files via upload

--- src/main/java/edu/eci/arep/Main.java
+++ src/main/java/edu/eci/arep/Main.java
@@ -4,6 +4,7 @@ package edu.eci.arep;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        HttpServer.start(new String[]{"edu.eci.arep.examples.HelloController"});
+        //HttpServer.start(new String[]{"edu.eci.arep.examples.HelloController"});
+        HttpServer.start(args);
     }
 }
